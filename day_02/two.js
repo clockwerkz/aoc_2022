a47384caeb76ed9f53751fc77600ca2326bee52e
clockwerkz <clockwerkz@gmail.com>
2022-12-03 20:07:38 -0800

    solved part 2 of day 2

--- day_02/two.js
+++ day_02/two.js
@@ -1,14 +1,15 @@
 /* A for Rock, B for Paper, C for Scissors
    X for Rock, Y for Paper, Z for Scissors
-
+   PART 2:
+   X: lose, Y: draw, Z: win
    score - loss: 0, draw: 3, win:  6
 */
 
 function match([p1, p2]) {
     if (p1 === 'A') {
-        if (p2.trim() === 'X') return 1 + 3;
-        if (p2.trim() === 'Y') return 2 + 6;
-        if (p2.trim() === 'Z') return 3 + 0;
+        if (p2.trim() === 'X') return 3 + 0;
+        if (p2.trim() === 'Y') return 1 + 3;
+        if (p2.trim() === 'Z') return 2 + 6;
     }
     if (p1 === 'B') {
         if (p2 === 'X') return 1 + 0;
@@ -16,9 +17,9 @@ function match([p1, p2]) {
         if (p2 === 'Z') return 3 + 6;
     }
     if (p1 === 'C') {
-        if (p2 === 'X') return 1 + 6;
-        if (p2 === 'Y') return 2 + 0;
-        if (p2 === 'Z') return 3 + 3;
+        if (p2 === 'X') return 2 + 0;
+        if (p2 === 'Y') return 3 + 3;
+        if (p2 === 'Z') return 1 + 6;
     }
 }
 
