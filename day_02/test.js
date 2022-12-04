55b4c271aa828b8bde25db70c4e85fff32410698
clockwerkz <clockwerkz@gmail.com>
2022-12-03 19:56:11 -0800

    day 2 part 1 complete

---
+++ day_02/test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+const one = require("./one");
+const two = require("./two");
+
+describe("Day # - Part 1",()=>{
+    const answer = 0;
+    it(`should return ${answer}`,()=>{
+        assert.strictEqual(one(), answer);
+    });
+});
+
+describe("Day # - Part 2",()=>{
+    const answer = 0;
+    it(`should return ${answer}`,()=>{
+        assert.strictEqual(two(), answer);
+    });
+});
